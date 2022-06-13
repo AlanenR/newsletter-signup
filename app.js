71ae02472c7307125f9875f0e7876112487224ca
Riikka Alanen <69243343+AlanenR@users.noreply.github.com>
2022-06-13 17:22:11 -0500

    Added and tested post method

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/signup.html");
   });
 
+app.post("/", function (req, res) {
+    const firstName = req.body.inputFirstName;
+    const lastName = req.body.inputLastName;
+    const email = req.body.inputEmail;
+    console.log(firstName + lastName + email)
+    res.send("Success");
+});
+
   app.listen("3000", function() {
       console.log("Server is running on port 3000");
   });
