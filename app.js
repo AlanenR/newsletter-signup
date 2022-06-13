0d8efa23497539e59ff731d9481a9fcfc3d43992
Riikka Alanen <69243343+AlanenR@users.noreply.github.com>
2022-06-13 16:56:22 -0500

    Added static files

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const app = express();
+const bodyParser = require('body-parser');
 const https = require("https");
 
-const bodyParser = require('body-parser');
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
-    res.send('hello world');
-  })
+    res.sendFile(__dirname + "/signup.html");
+  });
 
   app.listen("3000", function() {
       console.log("Server is running on port 3000");
-  })
+  });
